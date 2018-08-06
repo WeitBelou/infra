4283f8523a60160287722e89a57324adc42a4b85
Ivan Kosolapov <kosolapow.iwan@gmail.com>
2018-08-06 15:21:21 +0300

    Setup before gcloud

---
+++ README.md
@@ -0,0 +1,5 @@
+bastion_IP = 35.190.164.163
+someinternalhost_IP = 10.142.0.3
+
+testapp_IP = *
+testapp_port = *
--- terraform/main.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  version = "1.4.0"
-  project = "harbinger-212217"
-  region = "europe-west1"
-}
